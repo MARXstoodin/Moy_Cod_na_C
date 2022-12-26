ba088b305c8918cd5c5112945647b3ce7a3178e1
MARXstoodin <abramovarsenii45@gmail.com>
2022-12-26 19:09:37 +0300

    26.12.22

--- Musor/dyn_arr.c
+++ Musor/dyn_arr.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
 
+typedef struct 
+{
+	int* arr;
+	int cap;
+	int max;
+}din_arr;
+	
 int main()
 {
-	int* arr = malloc(40);
-	printf("%d ", arr[5]);
-	arr[5] = 5;
-	printf("%d", arr[5]);
+	int i;
+	din_arr array;
+	scanf("%d", &i);
+	scanf("%d", &array.arr[i]);
+	scanf("%d", &array.max);
+	append(array);
+	for (i=0; i < array.max ; i++)
+	{
+		printf("%d\n", array.arr[i]);
+	}
 	return 0;
+}
+
+void append(din_arr array, int new)
+{
+	while (array.cap < 1)
+	{
+		array.max++;
+		array.cap = array.max - *array.arr;
+		printf("array.max++;");
+	}
 }
