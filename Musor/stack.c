6c81be121a48d8f569e7183adeadb0f85512605c
MARXstoodin <abramovarsenii45@gmail.com>
2023-02-06 18:55:42 +0300

    moy_stack

--- Musor/stack.c
+++ Musor/stack.c
@@ -1,19 +1,44 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
-
-
-
-void upper()
+typedef struct node
 {
-	
-	re
+	int integer;
+	struct node* previos;
+}
+node;
+
+node* initiolase()
+{
+	node* a;
+	a = malloc(8);
+	a->previos = NULL;
+	return a;
 }
 
-int main()
+void push(node* a)
+{
+	scanf("%d", a->integer);
+	a->previos = (node*)a;
+}
+
+void pop()
 {
 	
+}
+
+void top()
+{
+	
+}
+
+int main(node a)
+{
+	initiolase();
+	
+	push(&a);
+	
+	print("i:%d p:%d\n", a.integer, a.previos);
 	
 	return 0;
 }
